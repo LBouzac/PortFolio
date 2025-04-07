043401171a31628a02b8b3edd4a4eaf18d1000ff
LBouzac <bouzacludovic@gmail.com>
2025-04-07 14:56:03 +0200

    modif projets

---
+++ src/app/cv/cv.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  imports: [],
+  templateUrl: './cv.component.html',
+  styleUrl: './cv.component.css'
+})
+export class CvComponent {
+
+}
