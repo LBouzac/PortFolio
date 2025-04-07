a5d1306576c67db90db0943a134f846ff93303d8
LBouzac <bouzacludovic@gmail.com>
2025-04-07 14:42:32 +0200

    ajout CV et Readme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 import {ProjetsComponent} from './projets/projets.component';
 import {ContactComponent} from './contact/contact.component';
+import {CvComponent} from './cv/cv.component';
 
 export const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'projets', component: ProjetsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'cv', component: CvComponent },
   { path: '', redirectTo: '/accueil', pathMatch: 'full' }
 ];
