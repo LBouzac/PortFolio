5edef965e45a48e32e60b85d34bdf2bcb65c09dd
ludo <bouzacludovic@gmail.com>
2026-04-04 17:37:47 +0200

    suppr de emailjs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import emailjs from '@emailjs/browser';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -11,39 +10,5 @@ import { environment } from '../../environments/environment';
   imports: [CommonModule]
 })
 export class ContactComponent {
-  loading = false;
-  success = false;
-  error   = false;
 
-  constructor() {}
-
-  async handleSubmit(event: Event) {
-    event.preventDefault();
-
-    const form = event.target as HTMLFormElement;
-    this.loading = true;
-    this.error = false;
-
-    try {
-      const result = await emailjs.sendForm(
-        environment.emailjs.serviceId,
-        environment.emailjs.templateId,
-        form,
-        environment.emailjs.publicKey
-      );
-
-      if (result.text === 'OK') {
-        this.success = true;
-        form.reset();
-      } else {
-        this.error = true;
-        console.error('Erreur d\'envoi:', result);
-      }
-    } catch (error) {
-      this.error = true;
-      console.error('Erreur d\'envoi:', error);
-    } finally {
-      this.loading = false;
-    }
-  }
 }
